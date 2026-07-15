be9d679cc2fa43a4d17b3a2d910c2b95df66c7a2
junh <junh001224@gmail.com>
2026-07-15 19:01:56 +0900

    1일차

---
+++ isaacpjt/basic/standalone_basic.py
@@ -0,0 +1,68 @@
+from isaacsim import SimulationApp
+simulation_app = SimulationApp({"headless": False})     # 1. Application
+
+import numpy as np
+import time
+import omni.usd
+from isaacsim.core.api import World
+from isaacsim.core.api.objects import DynamicCuboid
+import omni.timeline
+
+world = World(stage_units_in_meters=1.0)                # 2. World
+stage = omni.usd.get_context().get_stage()              # 3. Stage
+
+# cube_prim_blue = DynamicCuboid(                              # 4. Prim
+#     prim_path="/World/BlueCube",
+#     name="blue_cube",
+#     position=np.array([1.5, 0.0, 1.0]),
+#     scale=np.array([0.15, 0.15, 0.15]),
+#     color=np.array([0.0, 0.0, 1.0]),
+# )
+
+cube_prim_red = DynamicCuboid(                              # 4. Prim
+    prim_path="/World/RedCube",
+    name="red_cube",
+    position=np.array([0.0, 0.0, 1.0]),
+    scale=np.array([0.1, 0.1, 0.1]),
+    color=np.array([1.0, 0.0, 0.0]),
+)
+
+# cube_prim_green = DynamicCuboid(                              # 4. Prim
+#     prim_path="/World/GreenCube",
+#     name="green_cube",
+#     position=np.array([0.0, 0.0, 0.15]),
+#     scale=np.array([0.3, 0.3, 0.3]),
+#     color=np.array([0.0, 1.0, 0.0]),
+# )
+
+
+world.scene.add_default_ground_plane()                  # 5. Scene
+# world.scene.add(cube_prim_blue)
+world.scene.add(cube_prim_red)
+# world.scene.add(cube_prim_green)
+
+world.reset()
+count = 0
+was_playing = False
+
+while simulation_app.is_running():                      # 6. Simulation
+    world.step(render=True)
+    
+    is_playing = world.is_playing()
+    
+    if is_playing and not was_playing:
+        count = 0
+        print("시작 버튼 눌림: count 0으로 초기화")
+
+    if is_playing:
+        count += 1
+        
+        if (count % 100) == 0:
+            print(count)
+            if (count % 300) == 0:
+                cube_prim_red.set_world_pose(position=np.array([0.0, 0.0, 1.0]))
+                print('큐브 이동')
+                
+    was_playing = is_playing
+
+simulation_app.close()
