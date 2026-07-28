4e3e8691a887666394f5c2a34c5ca0c368d15146
junh0012 <junh001224@gmail.com>
2026-07-28 14:57:21 +0900

    3대 실행

--- isaacpjt/mainsim/main_sim.py
+++ isaacpjt/mainsim/main_sim.py
@@ -59,7 +59,7 @@ from amr_pick_place import AmrArmController, prepare_cone_triggers
 # amr_1의 로봇팔 pick&place 자동화만 우선 구현한다(다른 AMR로 확장하려면 이
 # 세트를 늘리면 된다) - amr_ready/amr_carrying_complete의 amr_id(int)와
 # AmrArmController를 매핑하는 데 쓴다.
-PICK_PLACE_ROBOT_IDS = ("amr_1", "amr_2")
+PICK_PLACE_ROBOT_IDS = ("amr_1", "amr_2", "amr_3")
 # pick/place 목표(demo0725.usd의 pallet/pallet_01/02/03)는 amr_pick_place.py가
 # 직접 읽으므로 여기서는 박스 크기만 필요하다.
 PICK_BOX_SIZE = np.array([0.28, 0.20, 0.11])
